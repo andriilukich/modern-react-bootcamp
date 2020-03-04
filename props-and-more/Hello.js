53732ddcdad9feee8129fc157afbd58e24faaf51
Andrii <andriilukich@gmail.com>
2020-03-04 09:57:11 +0200

    Other Types of Props

--- props-and-more/Hello.js
+++ props-and-more/Hello.js
@@ -1,6 +1,13 @@
 class Hello extends React.Component {
   render() {
     const props = this.props;
-    return <p>Hi {props.to} from {props.from}!</p>
+    let bangs = '!'.repeat(props.bangs);
+    console.log(props);
+    return (
+      <div>
+        <p>Hi {props.to} from {props.from}{bangs}</p>
+        <img src={props.img} />
+      </div>
+    )
   }
 }
