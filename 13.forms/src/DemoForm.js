a14ca4c0d3410ebbe71086f3f5ab94577fbb2a0d
Andrii <andriilukich@gmail.com>
2020-03-14 16:24:33 +0200

    The htmlFor Attribute

--- 13.forms/src/DemoForm.js
+++ 13.forms/src/DemoForm.js
@@ -25,19 +25,25 @@ class DemoForm extends Component {
       <div>
         <h1>Demo Form</h1>
         <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Name</label>
           <input 
+            id="username"
             type="text"
             name="username"
             onChange={this.handleChange}
             value={this.state.username}
           />
+          <label htmlFor="user-id">Email</label>
           <input 
+            id="user-id"
             type="email"
             name="email"
             onChange={this.handleChange}
             value={this.state.email}
           />
+          <label htmlFor="user-password">Password</label>
           <input 
+            id="user-password"
             type="password"
             name="password"
             onChange={this.handleChange}
