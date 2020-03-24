905b20f23eaea3ad81067dfefeb7b061ae68b39e
Andrii <andriilukich@gmail.com>
2020-03-24 15:40:29 +0200

    Adding Animated Loaders

--- 17.lifecycle/src/ZenQuotes.js
+++ 17.lifecycle/src/ZenQuotes.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './ZenQuotes.css';
 
 class ZenQuotes extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: '' };
+    this.state = { quote: '', isLoaded: true};
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/zen').then(response => {
-      // console.log(response);
-      this.setState({ quote: response.data });
+      setTimeout(() => {
+      this.setState({ quote: response.data, isLoaded: false });
+      }, 3000);
     });
   }
 
   render() {
     return (
       <div>
-        <h1>Always remember...</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div className="loader">
+            <div className="loader-inner">
+              <div className="loader-line-wrap">
+                <div className="loader-line"></div>
+              </div>
+              <div className="loader-line-wrap">
+                <div className="loader-line"></div>
+              </div>
+              <div className="loader-line-wrap">
+                <div className="loader-line"></div>
+              </div>
+              <div className="loader-line-wrap">
+                <div className="loader-line"></div>
+              </div>
+              <div className="loader-line-wrap">
+                <div className="loader-line"></div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1>Always remember...</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        )}
       </div>
     );
   }
