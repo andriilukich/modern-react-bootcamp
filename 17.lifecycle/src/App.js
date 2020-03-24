d5cc7e9cb5fd4a50dc11d9c50793a75f6c80f983
Andrii <andriilukich@gmail.com>
2020-03-24 15:25:57 +0200

    Loading Data Via AJAX

--- 17.lifecycle/src/App.js
+++ 17.lifecycle/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Timer from './Timer';
+import ZenQuotes from './ZenQuotes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      {/* <Timer /> */}
+      <ZenQuotes />
     </div>
   );
 }
