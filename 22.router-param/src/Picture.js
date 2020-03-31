16ab6524865bf2f592afd64ad813ad065cf6de4d
Andrii Lukich <andriilukich@gmail.com>
2020-03-31 12:53:48 +0200

    The Redirect Component

--- 22.router-param/src/Picture.js
+++ 22.router-param/src/Picture.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Picture extends Component {
-  render() {
-    const name = this.props.match.params.name;
-    const API_URL = `https://source.unsplash.com/1600x900/?${name}`;
-    return (
-      <div className='Picture'>
-        <h1>This picture is about: {name}</h1>
-        <img src={API_URL} alt={name} style={{
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          width: 'auto',
-          height: '100vh'
-        }}/>
-      </div>
-    )
+    render() {
+      const name = this.props.match.params.name;
+      const API_URL = `https://source.unsplash.com/1600x900/?${name}`;
+      return (
+        <div className='Picture'>
+          {/\d/.test(name) ? <Redirect to='/'/> : (
+            <div>
+              <h1>This picture is about: {name}</h1>
+              <img src={API_URL} alt={name} style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                width: 'auto',
+                height: '100vh'
+              }}/>
+            </div>
+          )}
+        </div>
+      )
+    }
   }
-}
 
 export default Picture;
